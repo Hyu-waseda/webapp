c203f22621f96a5d68b7e210452adda7090c3de3
Hyu-waseda <okumura.hyu@akane.waseda.jp>
2022-10-29 14:37:11 +0900

    jest の導入

---
+++ frontend/__tests__/pages/index.test.tsx
@@ -0,0 +1,11 @@
+import { render } from "@testing-library/react";
+import Home from "../../src/pages";
+
+describe("テスト", () => {
+	it("テストのテスト", () => {
+		const IndexResult = render(
+			<Home />
+		)
+		expect(IndexResult).toMatchSnapshot();
+	})
+})

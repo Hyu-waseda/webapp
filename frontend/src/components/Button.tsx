58950ad89533f0f169c314dd27c1654ae292883d
Hyu-waseda <okumura.hyu@akane.waseda.jp>
2022-11-05 17:05:16 +0900

    sassの導入

---
+++ frontend/src/components/Button.tsx
@@ -0,0 +1,8 @@
+import { ReactElement } from "react";
+import styles from "components/Button.module.scss";
+
+export const Button = (): ReactElement<any, any> => {
+  return (
+    <button className={styles.button}>hgoe</button>
+  );
+};
